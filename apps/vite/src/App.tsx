77196a2ef41570490de2e3a39db0461e0d20081a
Chris Bautista <chrisbautista@netflix.com>
2023-04-04 15:54:51 -0700

    fix: vite await import

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,6 +1,9 @@
 import { fn, sum } from "@trash-oss/esm-package";
 import "./App.css";
 
+// examples for if you dont support esm
+const x = await import("@trash-oss/esm-package");
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +11,7 @@ function App() {
 
       <div>{sum(1, 2)}</div>
       <div>{fn()}</div>
+      <div>{x.fn()}</div>
     </div>
   );
 }
